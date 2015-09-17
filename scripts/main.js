4eba71eb37beb5cd3368ae8db8fe5e81c8ecf8c2
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2015-09-17 23:20:52 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,16 @@ var app = angular.module('app', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
     $scope.people = [{
-        name: 'Naomi',
+        name: 'Girl1',
         pic: 'https://goo.gl/t7GDwd',
         value: '1'
     }, {
-        name: 'Dana',
-        pic: 'https://goo.gl/D0nCGh',
+        name: 'Girl2',
+        pic: 'http://dreamatico.com/data_images/girl/girl-8.jpg',
         value: '2'
     }, {
-        name: 'Sasha',
-        pic: 'https://goo.gl/q65DUV',
+        name: 'Girl3',
+        pic: 'http://cdn.playbuzz.com/cdn/837956f3-756f-4901-b151-a72faa7e86f1/26d40aab-d042-428e-a0ff-7f12eb4718d1.jpg',
         value: '3'
     }];
     $scope.params = ["Who has the nicest BUTT", "Who has the best TITS","Who has the sexiest LEGS","Who has the most beautiful FACE","Who has the best CHARACTER","Who do you have the best EXPERIENCE with","Who do you share the most COMMON INTERESTS with"]
